bf4d1a303b38bb9d3e5a13171500b7630c309d2f
Stephen Moore <smoore@capstonedigital.com>
2014-04-04 17:31:59 -0500

    About to deploy

--- app/Config/database.php
+++ app/Config/database.php
@@ -4,17 +4,17 @@ class DATABASE_CONFIG {
 	public $test = array(
 		'datasource' => 'Database/Mysql',
 		'persistent' => false,
-		'host' => '127.0.0.1',
-		'login' => 'root',
-		'password' => 'gsnyder56',
+		'host' => 'mooredatabase56.cd02yxrr7fxm.us-east-1.rds.amazonaws.com',
+		'login' => 'birding_readonly',
+		'password' => 'dY78vNqP37sS94U',
 		'database' => 'birding',
 	);
 	public $default = array(
 		'datasource' => 'Database/Mysql',
 		'persistent' => false,
-		'host' => '127.0.0.1',
-		'login' => 'root',
-		'password' => 'gsnyder56',
+		'host' => 'mooredatabase56.cd02yxrr7fxm.us-east-1.rds.amazonaws.com',
+		'login' => 'birding_readonly',
+		'password' => 'dY78vNqP37sS94U',
 		'database' => 'birding',
 	);
 }
--- app/Controller/TestsController.php
+++
@@ -1,3 +0,0 @@
-class TestsController extends AppController {
-
-}
