e26b1104017b4606d5380d1096922b12faea05c6
Stephen Moore <smoore@capstonedigital.com>
2014-04-11 11:16:37 -0500

    Database config changed to prod

--- app/Config/database.php
+++ app/Config/database.php
@@ -1,37 +1,37 @@
 <?php
 class DATABASE_CONFIG {
 
-	// public $test = array(
-		// 'datasource' => 'Database/Mysql',
-		// 'persistent' => false,
-		// 'host' => 'mooredatabase56.cd02yxrr7fxm.us-east-1.rds.amazonaws.com',
-		// 'login' => 'birding_readonly',
-		// 'password' => 'dY78vNqP37sS94U',
-		// 'database' => 'birding',
-	// );
-	// public $default = array(
-		// 'datasource' => 'Database/Mysql',
-		// 'persistent' => false,
-		// 'host' => 'mooredatabase56.cd02yxrr7fxm.us-east-1.rds.amazonaws.com',
-		// 'login' => 'birding_readonly',
-		// 'password' => 'dY78vNqP37sS94U',
-		// 'database' => 'birding',
-	// );
-	public $default = array(
-		'datasource' => 'Database/Mysql',
-		'persistent' => false,
-		'host' => '127.0.0.1',
-		'login' => 'root',
-		'password' => 'gsnyder56',
-		'database' => 'birding',
-	);
 	public $test = array(
 		'datasource' => 'Database/Mysql',
 		'persistent' => false,
-		'host' => '127.0.0.1',
-		'login' => 'root',
-		'password' => 'gsnyder56',
+		'host' => 'mooredatabase56.cd02yxrr7fxm.us-east-1.rds.amazonaws.com',
+		'login' => 'birding_readonly',
+		'password' => 'dY78vNqP37sS94U',
 		'database' => 'birding',
-	);	
+	);
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'mooredatabase56.cd02yxrr7fxm.us-east-1.rds.amazonaws.com',
+		'login' => 'birding_readonly',
+		'password' => 'dY78vNqP37sS94U',
+		'database' => 'birding',
+	);
+	// public $default = array(
+		// 'datasource' => 'Database/Mysql',
+		// 'persistent' => false,
+		// 'host' => '127.0.0.1',
+		// 'login' => 'root',
+		// 'password' => 'gsnyder56',
+		// 'database' => 'birding',
+	// );
+	// public $test = array(
+		// 'datasource' => 'Database/Mysql',
+		// 'persistent' => false,
+		// 'host' => '127.0.0.1',
+		// 'login' => 'root',
+		// 'password' => 'gsnyder56',
+		// 'database' => 'birding',
+	// );	
 		
 }
