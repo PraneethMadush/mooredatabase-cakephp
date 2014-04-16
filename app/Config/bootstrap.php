e816965f46c0b20c314e1bc3ad49c48c76dfe441
Stephen Moore <stephenmoore56@msn.com>
2014-04-16 14:05:48 -0500

    Configuring for memcached

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -24,8 +24,18 @@
 
 // Setup a 'default' cache configuration for use in the application.
 // Cache::config('default', array('engine' => 'File'));
-Cache::config('default', array('engine' => 'Apc'));
-
+// Cache::config('default', array('engine' => 'Apc'));
+Cache::config('default', array(
+	'engine' => 'Memcache', //[required]
+	'duration' => 3600000, //[optional]
+	'probability' => 100, //[optional]
+//	'prefix' => Inflector::slug(APP_DIR) . '_', //[optional]  prefix every cache file with this string
+	'servers' => array(
+	'127.0.0.1:11211' // localhost, default port 11211
+	), //[optional]
+	'persistent' => true, // [optional] set this to false for non-persistent connections
+	'compress' => false, // [optional] compress data in Memcache (slower, but uses less memory)
+));
 /**
  * The settings below can be used to set additional paths to models, views and controllers.
  *
