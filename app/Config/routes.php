f6a38a4e5e14f938a8b6822a333f901599a6a2cc
Stephen Moore <stephenmoore56@msn.com>
2016-04-12 18:38:52 -0500

    Remove unused routes

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -13,34 +13,31 @@
  * For full copyright and license information, please see the LICENSE.txt
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link          http://cakephp.org CakePHP(tm) Project
  * @package       app.Config
- * @since         CakePHP(tm) v 0.2.9
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ *
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @since         CakePHP(tm) v 0.2.9
  */
 /**
  * Here, we are connecting '/' (base path) and some other permalinks to controller called 'Pages',
  * its action called 'display', and we pass a param to select the view file
  * to use (in this case, /app/View/Pages/home.ctp)...
  */
-Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
-Router::connect('/aboutme', array('controller' => 'pages', 'action' => 'display', 'aboutme'));
-Router::connect('/architecture', array('controller' => 'pages', 'action' => 'display', 'architecture'));
-Router::connect('/canvas', array('controller' => 'pages', 'action' => 'display', 'canvas'));
-Router::connect('/education', array('controller' => 'pages', 'action' => 'display', 'education'));
-Router::connect('/geolocation', array('controller' => 'pages', 'action' => 'display', 'geolocation'));
-Router::connect('/home', array('controller' => 'pages', 'action' => 'display', 'home'));
-Router::connect('/settings', array('controller' => 'pages', 'action' => 'display', 'settings'));
-Router::connect('/tools', array('controller' => 'pages', 'action' => 'display', 'tools'));
-Router::connect('/training', array('controller' => 'pages', 'action' => 'display', 'training'));
-Router::connect('/youtube', array('controller' => 'pages', 'action' => 'display', 'youtube'));
-// we need a redirect here or the click on photo to return to set feature won't work
-Router::redirect('/flickr', array('controller' => 'pages', 'action' => 'display', 'flickrsets'));
+Router::connect('/', ['controller' => 'pages', 'action' => 'display', 'home']);
+Router::connect('/aboutme', ['controller' => 'pages', 'action' => 'display', 'aboutme']);
+Router::connect('/architecture', ['controller' => 'pages', 'action' => 'display', 'architecture']);
+Router::connect('/education', ['controller' => 'pages', 'action' => 'display', 'education']);
+Router::connect('/geolocation', ['controller' => 'pages', 'action' => 'display', 'geolocation']);
+Router::connect('/home', ['controller' => 'pages', 'action' => 'display', 'home']);
+Router::connect('/tools', ['controller' => 'pages', 'action' => 'display', 'tools']);
+Router::connect('/training', ['controller' => 'pages', 'action' => 'display', 'training']);
 /**
  * ...and connect the rest of 'Pages' controller's URLs.
  */
-Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
+Router::connect('/pages/*', ['controller' => 'pages', 'action' => 'display']);
 
 /**
  * Load all plugin routes. See the CakePlugin documentation on
